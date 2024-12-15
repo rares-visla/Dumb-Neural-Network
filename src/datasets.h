ed05a94b79b4ad14aa19dd25bf66450ba5cc8aa4
Rares Visla <rares.visla@gmail.com>
2024-12-15 11:10:52 +0100

    a bit of dataset handling

--- src/datasets.h
+++ src/datasets.h
@@ -1,5 +1,6 @@
 #pragma once
 
+//{input, output}
 float train_doubling[][2] = {
     {0,0},
     {1,2},
--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include "neuralnet.h"
 #include "datasets.h"
 
-#define NUM_INPUTS 2
+#define NUM_INPUTS 1
 #define NUM_LAYERS 1
 #define NUM_NEURONS 1 //neurons per layer
 
@@ -30,9 +30,37 @@ float rand_float()
 int main(int argc, char const *argv[])
 {
     srand(time(0));
+
+    //another idea is to make NUM_INPUTS equal to (sizeof(*train_doubling) / sizeof(float)) - 1 beforehand
+    if (NUM_INPUTS != ((sizeof(*train_doubling) / sizeof(float)) - 1)) // -1 because 1 element is the output
+    {
+        fprintf(stderr,"NUMBER OF DECLARED INPUTS DIFFERS FROM NUMBER OF INPUTS IN THE DATASET");
+        exit(1);
+    }
+
+    float (*dataset)[NUM_INPUTS + 1] = train_doubling; // +1 to accomodate the output
+
     Neuralnet simple_nn = initialize_nn(NUM_LAYERS, NUM_NEURONS, NUM_INPUTS);
 
+
     print_nn(&simple_nn);
+
+    //forward propagation
+    // float y = 0;
+    // for (size_t i = 0; i < simple_nn.num_layers; i++)
+    // {
+    //     for (size_t j = 0; j < simple_nn.layers[i].num_neurons; j++)
+    //     {
+    //         Neuron *neuron = &simple_nn.layers[i].neurons[j];
+    //         for (size_t k = 0; k < neuron->num_weights; k++)
+    //         {
+    //             y += neuron->w[k] * 
+    //         }
+            
+    //     }
+        
+    // }
+    
 
     free_nn(&simple_nn);
 
